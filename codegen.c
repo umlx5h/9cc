d4bafd078279f509fd0018b99e69de2f83a7776d
umlx5h <umlx5h21@protonmail.com>
2023-09-05 22:44:55 +0900

    Make sure that stack frames are aligned to 16 bytes boundaries
    * ビット演算をして16の倍数チェックを行って、必要に応じてRSPを調整

--- codegen.c
+++ codegen.c
@@ -1,17 +1,5 @@
 #include "chibicc.h"
 
-bool needed_rsp_align;
-int offset;
-
-void flip_rsp_align(bool is_push) {
-  if (is_push) {
-    offset += 8;
-  } else {
-    offset -= 8;
-  }
-  needed_rsp_align = !needed_rsp_align;
-}
-
 int labelseq = 0;
 char *argreg[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 
@@ -20,7 +8,6 @@ void gen_addr(Node *node) {
   if (node->kind == ND_VAR) {
     printf("  lea rax, [rbp-%d]\n", node->var->offset);
     printf("  push rax\n");
-    flip_rsp_align(true);
     return;
   }
 
@@ -38,7 +25,6 @@ void store() {
   printf("  pop rax\n");
   printf("  mov [rax], rdi\n");
   printf("  push rdi\n");
-  flip_rsp_align(false);
 }
 
 // Generate code for a given node.
@@ -46,12 +32,10 @@ void gen(Node *node) {
   switch (node->kind) {
   case ND_NUM:
     printf("  push %d\n", node->val);
-    flip_rsp_align(true);
     return;
   case ND_EXPR_STMT:
     gen(node->lhs);
     printf("  add rsp, 8\n"); // 式の評価結果としてスタックに一つの値が残っているのでポップしておく (rspを加算する)
-    flip_rsp_align(false);
     return;
   case ND_VAR: // 右辺に変数が現れた時にメモリからレジスタにコピーして1つの値にしてスタックにpush
     gen_addr(node);
@@ -67,7 +51,6 @@ void gen(Node *node) {
     if (node->els) {
       gen(node->cond);
       printf("  pop rax\n");
-      flip_rsp_align(false);
       printf("  cmp rax, 0\n");
       printf("  je  .Lelse%d\n", seq);
       gen(node->then);
@@ -78,7 +61,6 @@ void gen(Node *node) {
     } else {
       gen(node->cond);
       printf("  pop rax\n");
-      flip_rsp_align(false);
       printf("  cmp rax, 0\n");
       printf("  je  .Lend%d\n", seq);
       gen(node->then);
@@ -91,7 +73,6 @@ void gen(Node *node) {
     printf(".Lbegin%d:\n", seq);
     gen(node->cond);
     printf("  pop rax\n");
-    flip_rsp_align(false);
     printf("  cmp rax, 0\n");
     printf("  je  .Lend%d\n", seq);
     gen(node->then);
@@ -107,7 +88,6 @@ void gen(Node *node) {
     if (node->cond) {
       gen(node->cond);
       printf("  pop rax\n");
-      flip_rsp_align(false);
       printf("  cmp rax, 0\n");
       printf("  je  .Lend%d\n", seq);
     }
@@ -128,31 +108,35 @@ void gen(Node *node) {
       gen(arg);
       nargs++;
     }
-
-    for (int i = nargs - 1; i >= 0; i--) {
+    for (int i = nargs - 1; i >= 0; i--)
       printf("  pop %s\n", argreg[i]);
-      flip_rsp_align(false);
-    }
-
-    if (needed_rsp_align) {
-      // 16 byte alignment
-      printf("  sub rsp, 8\n");
-    }
-
+    
+    // スタックポインタを調整する
+    // pushとpopは8バイトRSPを増減させるが、関数呼び出し時にRSPは16の倍数になってないといけない
+    // このため16の倍数であるかを確認し、そうでない場合は8の倍数と仮定しRSPを8バイト分押し下げてからcallする
+    // We need to align RSP to a 16 byte boundary before
+    // calling a function because it is an ABI requirement.
+    // RAX is set to 0 for variadic function.
+    int seq = labelseq++;
+    printf("  mov rax, rsp\n");
+    printf("  and rax, 15\n"); // 0x1111 とANDを取り下位4ビットを取り出す
+    printf("  jnz .Lcall%d\n", seq); // rspの下位4ビットがゼロではなかったら、スタックポインタの調整をするためジャンプ
+    printf("  mov rax, 0\n");
     printf("  call %s\n", node->funcname);
-
-    if (needed_rsp_align) {
-      printf("  add rsp, 8\n");
-    }
+    printf("  jmp .Lend%d\n", seq);
+    printf(".Lcall%d:\n", seq);
+    printf("  sub rsp, 8\n"); // 8バイト押し下げておく
+    printf("  mov rax, 0\n");
+    printf("  call %s\n", node->funcname);
+    printf("  add rsp, 8\n"); // 戻す
+    printf(".Lend%d:\n", seq);
     printf("  push rax\n"); // 関数の返り値をスタックに積む
-    flip_rsp_align(true);
     return;
   }
   case ND_RETURN:
     gen(node->lhs);
     printf("  pop rax\n");
     printf("  jmp .Lreturn\n");
-    flip_rsp_align(false);
     return;
   }
 
@@ -204,7 +188,6 @@ void gen(Node *node) {
   }
 
   printf("  push rax\n");
-  flip_rsp_align(true);
 }
 
 void codegen(Program *prog) {
@@ -217,13 +200,6 @@ void codegen(Program *prog) {
   printf("  push rbp\n");
   printf("  mov rbp, rsp\n");
   printf("  sub rsp, %d\n", prog->stack_size);
-
-  offset = prog->stack_size;
-  if (prog->stack_size % 16 == 0) {
-    needed_rsp_align = false;
-  } else {
-    needed_rsp_align = true;
-  }
 
   // Emit code
   for (Node *node = prog->node; node; node = node->next)
